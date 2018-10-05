9978617c3c902cb39911b97ac26bc579f0258526
Adam Juhos <ajuhos@logiscool.com>
2018-10-05 11:57:55 +0200

    Do not build unused code generators

--- build.py
+++ build.py
@@ -229,11 +229,6 @@ class Gen_compressed(threading.Thread):
     self.gen_blocks("horizontal")
     self.gen_blocks("vertical")
     self.gen_blocks("common")
-    self.gen_generator("javascript")
-    self.gen_generator("python")
-    self.gen_generator("php")
-    self.gen_generator("dart")
-    self.gen_generator("lua")
 
   def gen_core(self, vertical):
     if vertical:
